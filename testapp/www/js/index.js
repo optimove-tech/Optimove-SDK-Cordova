57717cd16c2d6516fb0bb5a4f938d715b5743672
Barak <Barak_b@optimove.com>
2022-06-23 12:25:58 +0300

    add test app

---
+++ testapp/www/js/index.js
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+
+    optimobileswift.initialize()
+}
+
+document.getElementById("Set Install ID").addEventListener("click", setInstallID);
+
+function setInstallID() {
+    optimobileswift.setInstallId("123456")
+}
+
+document.getElementById("push Campaigns Is Enabled").addEventListener("click", campaignsIsEnabled);
+
+function campaignsIsEnabled() {
+    optimobileswift.pushCampaignsIsEnabled()
+}
+document.getElementById("get Inbox Items").addEventListener("click", getInbox);
+
+function getInbox() {
+    optimobileswift.getInboxItems()
+}
+
