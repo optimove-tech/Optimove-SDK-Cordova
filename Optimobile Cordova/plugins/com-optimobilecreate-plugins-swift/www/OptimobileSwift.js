bec8b51c1a9194cf16a5d2e5e73a4992f03b817e
Barak <Barak_b@optimove.com>
2022-06-27 15:32:54 +0300

    add optimove public api calls

--- Optimobile Cordova/plugins/com-optimobilecreate-plugins-swift/www/OptimobileSwift.js
+++ Optimobile Cordova/plugins/com-optimobilecreate-plugins-swift/www/OptimobileSwift.js
@@ -1,49 +1,52 @@
-var exec = require('cordova/exec');
-
-exports.setInstallId = function (arg0, success, error) {
-    exec(success, error, 'OptimobileSwift', 'setInstallId', [arg0]);
-};
-
-exports.initialize = function (arg0, success, error) {
-    exec(success, error, 'OptimobileSwift', 'initialize', [arg0]);
-};
-
-exports.pushCampaignsIsEnabled = function (arg0, success, error) {
-    exec(success, error, 'OptimobileSwift', 'pushCampaignsIsEnabled', [arg0]);
-};
-
-exports.getInboxItems = function (arg0, success, error) {
-    exec(success, error, 'OptimobileSwift', 'getInboxItems', [arg0]);
-};
-
-exports.setInstallIdjs = function(arg0, success, error) {
-    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-      success(arg0);
-    } else {
-      error('Empty message!');
-    }
+cordova.define("com-optimobilecreate-plugins-swift.OptimobileSwift", function(require, exports, module) {
+  var exec = require('cordova/exec');
+  
+  exports.setInstallId = function (arg0, success, error) {
+      exec(success, error, 'OptimobileSwift', 'setInstallId', [arg0]);
   };
-
-  exports.initializejs = function(arg0, success, error) {
-    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-      success(arg0);
-    } else {
-      error('Empty message!');
-    }
+  
+  exports.initialize = function (arg0, arg1, success, error) {
+      exec(success, error, 'OptimobileSwift', 'initialize', [arg0, arg1]);
   };
-
-  exports.pushCampaignsIsEnabledjs = function(arg0, success, error) {
-    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-      success(arg0);
-    } else {
-      error('Empty message!');
-    }
+  
+  exports.pushCampaignsIsEnabled = function (arg0, success, error) {
+      exec(success, error, 'OptimobileSwift', 'pushCampaignsIsEnabled', [arg0]);
   };
-
-  exports.getInboxItemsjs = function(arg0, success, error) {
-    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-      success(arg0);
-    } else {
-      error('Empty message!');
-    }
+  
+  exports.getInboxItems = function (arg0, success, error) {
+      exec(success, error, 'OptimobileSwift', 'getInboxItems', [arg0]);
   };
+  
+  exports.setInstallIdjs = function(arg0, arg1, success, error) {
+    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+        success(arg0);
+      } else {
+        error('Empty message!');
+      }
+    };
+  
+    exports.initializejs = function(arg0, success, error) {
+      if (arg0 && typeof(arg0) === 'string' && arg0.length > 0 && arg1 && typeof(arg1) === 'string' && arg1.length > 0) {
+        success(arg0);
+      } else {
+        error('Empty message!');
+      }
+    };
+  
+    exports.pushCampaignsIsEnabledjs = function(arg0, success, error) {
+      if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+        success(arg0);
+      } else {
+        error('Empty message!');
+      }
+    };
+  
+    exports.getInboxItemsjs = function(arg0, success, error) {
+      if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+        success(arg0);
+      } else {
+        error('Empty message!');
+      }
+    };
+  });
+  
