5bdc35df0e6b0bfb37b3ff7331e1149fe4291247
Barak <Barak_b@optimove.com>
2022-07-26 15:05:10 +0300

    - add OptimoveSDK import
    - add class declaration that was missing
    
    - create getCredentials helper function
      that parse to json file crated in file system from java script code
      to get OptimoveCredentials and OptimoveMobileCredentials
      for SDK initializintion

--- Cordova_QA_App/platforms/ios/Cordova_QA_App/Plugins/com-optimobilecreate-plugins/OptimobileSDKPlugin.swift
+++ Cordova_QA_App/platforms/ios/Cordova_QA_App/Plugins/com-optimobilecreate-plugins/OptimobileSDKPlugin.swift
@@ -1,14 +1,34 @@
-@objc(initialize:)
-  func initialize(command: CDVInvokedUrlCommand) {
-    let args = command.arguments
+import OptimoveSDK
+@objc(Optimobile_Cordova) class OptimobileSDKPlugin : CDVPlugin {
+    private let optimoveCredentialsKey = "optimove_credentials"
+    private let optimoveMobileCredentialsKey = "optimove_mobile_credentials"
     
-    let inAppConsentStrategy: InAppConsentStrategy = {
-      if let strategy = args?[2] as? String {
-        return .init(rawValue: strategy)!
-      }
-      return.notEnabled
-    }()
-
-    let config = OptimoveConfigBuilder(optimoveCredentials: args?[0] as? String, optimobileCredentials: args?[1] as? String).enableInAppMessaging(inAppConsentStrategy: inAppConsentStrategy).build()
-    Optimove.initialize(with: config)
-  }
+    @objc(initialize:)
+    func initialize(command: CDVInvokedUrlCommand) {
+        let args = command.arguments
+        
+        let inAppConsentStrategy: InAppConsentStrategy = {
+            if let strategy = args?[2] as? String {
+                return .init(rawValue: strategy)!
+            }
+            return.notEnabled
+        }()
+        
+        let config = OptimoveConfigBuilder(optimoveCredentials: args?[0] as? String, optimobileCredentials: args?[1] as? String).enableInAppMessaging(inAppConsentStrategy: inAppConsentStrategy).build()
+        Optimove.initialize(with: config)
+    }
+    
+    private func getCredentials(path: String) -> [String]? {
+        if let data = path.data(using: .utf8) {
+            do {
+                guard let jsonToDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String] else { return nil }
+                guard let optimoveCredentials = jsonToDict[optimoveCredentialsKey] else { return nil }
+                guard let optimoveMobileCredentials = jsonToDict[optimoveMobileCredentialsKey] else { return nil }
+                return [optimoveCredentials, optimoveMobileCredentials]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+}
