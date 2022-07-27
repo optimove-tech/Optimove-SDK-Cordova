a8c9dfe101305cb2d9a5452a90245eec521a6761
Tom Barzilay <tom_b@optimove.com>
2022-07-27 15:49:06 +0300

    delete platforms and plugins dirs

--- Example_App/plugins/optimove-cordova-plugin/www/Optimove.js
+++
@@ -1,8 +0,0 @@
-
-
-var exec = require('cordova/exec');
-
-
-window.initialize = function (inAppConsentStrategy, success, error) {
-    exec(success, error, 'OptimoveSDKPlugin', 'initialize', [inAppConsentStrategy]);
-};
