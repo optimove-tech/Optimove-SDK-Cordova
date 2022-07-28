b787891692549e43b48bff9b46b2a85d2a32def7
Barak <Barak_b@optimove.com>
2022-07-28 11:50:16 +0300

    branch off dev remove unnecessary testapp and optimobile folders

--- Optimobile Cordova/platforms/ios/Pods/OptimoveCore/OptimoveCore/Sources/Classes/Constants/SDKVersion.swift
+++
@@ -1,3 +0,0 @@
-//  Copyright © 2019 Optimove. All rights reserved.
-
-public let SDKVersion = "5.0.0"
--- testapp/platforms/ios/Pods/OptimoveCore/OptimoveCore/Sources/Classes/Extension/TimeInterval+Seconds.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2020 Optimove. All rights reserved.
-
-import Foundation
-
-public extension TimeInterval {
-    var seconds: Int64 {
-        return Int64(self)
-    }
-}
