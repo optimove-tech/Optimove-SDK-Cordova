8d205443993b578d57d85d895535909136e03c73
Barak <Barak_b@optimove.com>
2022-07-28 19:22:34 +0300

    - Change Initialize to read from config file - Initialize SDK on plugin load

--- plugins/src/ios/OptimoveSDKPlugin.swift
+++ plugins/src/ios/OptimoveSDKPlugin.swift
@@ -1,38 +1,51 @@
 import OptimoveSDK
+import NotificationCenter
 @objc(Optimobile_Cordova) class OptimobileSDKPlugin : CDVPlugin {
-    private let optimoveCredentialsKey = "optimove_credentials"
-    private let optimoveMobileCredentialsKey = "optimove_mobile_credentials"
+    private let internalQueue = DispatchQueue(label: "com.singletioninternal.queue",
+                                              qos: .default,
+                                              attributes: .concurrent)
+    
+    private var pendingPush: PushNotification?
+    
+    private let optimoveCredentialsKey = "optimoveCredentials"
+    private let optimoveMobileCredentialsKey = "optimoveMobileCredentials"
     
     private let credentialsJsonName = "optimove"
     
-    @objc(initialize:)
-    func initialize(command: CDVInvokedUrlCommand) {
-        guard let credentials = getCredentials(name: credentialsJsonName) else { return }
-        let args = command.arguments
-        let inAppConsentStrategy: InAppConsentStrategy = {
-            if let strategyName = args?[0] as? String {
-                if let strategy: InAppConsentStrategy = .init(rawValue: strategyName) {
-                    return strategy
-                }
-            }
-            return.notEnabled
-        }()
-        let config = OptimoveConfigBuilder(optimoveCredentials: credentials[0], optimobileCredentials: credentials[1]).enableInAppMessaging(inAppConsentStrategy: inAppConsentStrategy).build()
-        Optimove.initialize(with: config)
+    func load() {
+        internalQueue.sync(flags: .barrier) {
+            NotificationCenter.default.addObserver(self, selector: #selector(self.didFinishLaunching(notification:)), name: UIApplication.didFinishLaunchingNotification, object: nil)
+        }
     }
     
-    private func getCredentials(name: String) -> [String]? {
-        guard let url = Bundle.main.url(forResource: name, withExtension: "json") else {return nil }
-        if let data = try? Data(contentsOf: url) {
-            do {
-                guard let jsonToDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String] else { return nil }
-                guard let optimoveCredentials = jsonToDict[optimoveCredentialsKey] else { return nil }
-                guard let optimoveMobileCredentials = jsonToDict[optimoveMobileCredentialsKey] else { return nil }
-                return [optimoveCredentials, optimoveMobileCredentials]
-            } catch {
-                print(error.localizedDescription)
+    @objc func didFinishLaunching(notification: NSNotification) {
+        let configPath = Bundle.main.path(forResource: "optimove", ofType: "plist")
+        
+        guard let configPath = configPath else {
+            print("optimove.plist NOT FOUND");
+            return
+        }
+        
+        guard let configValues: [String: String] = NSDictionary(contentsOfFile: configPath) as? [String: String] else {
+            print("optimove.plist IS NOT VALID");return
+        }
+        
+        if let userInfo = notification.userInfo {
+            let userInfoDict = notification.userInfo?[UIApplication.LaunchOptionsKey.remoteNotification];
+            if userInfoDict != nil {
+                pendingPush = PushNotification.init(userInfo: userInfo, response: nil)
             }
         }
-        return nil
+        
+        let config = OptimoveConfigBuilder(optimoveCredentials: configValues[optimoveCredentialsKey], optimobileCredentials: configValues[optimoveMobileCredentialsKey])
+        
+        if configValues["inAppConsentStrategy"] == "auto-enroll" {
+            config.enableInAppMessaging(inAppConsentStrategy: .autoEnroll)
+        }
+        else if configValues["inAppConsentStrategy"] == "explicit-by-user" {
+            config.enableInAppMessaging(inAppConsentStrategy: .explicitByUser)
+        }
+        
+        Optimove.initialize(with: config.build())
     }
 }
