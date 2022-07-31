114ec38e8de18f0c10a432a9a9e5b131d7611d03
Barak <Barak_b@optimove.com>
2022-07-31 11:55:46 +0300

    - change optimoveMobileCredentialsKey - change optimoveCredentialsKey
    -------------- Add Rest Of Public Api ---------------
    - add function updateConsent
    - add function reportEvent
    - add function reportScreenVisit
    - add function registerUser
    - add function setUserId
    - add function getVisitorID
    - add function setUserEmail
    - add function getInboxItems
    - add function isAvailable

--- plugins/src/ios/OptimoveSDKPlugin.swift
+++ plugins/src/ios/OptimoveSDKPlugin.swift
@@ -1,14 +1,15 @@
 import OptimoveSDK
 import NotificationCenter
 @objc(Optimobile_Cordova) class OptimobileSDKPlugin : CDVPlugin {
+    
     private let internalQueue = DispatchQueue(label: "com.singletioninternal.queue",
                                               qos: .default,
                                               attributes: .concurrent)
     
     private var pendingPush: PushNotification?
     
-    private let optimoveCredentialsKey = "optimoveCredentials"
-    private let optimoveMobileCredentialsKey = "optimoveMobileCredentials"
+    private let optimoveCredentialsKey = "optimove_credentials"
+    private let optimoveMobileCredentialsKey = "optimove_mobile_credentials"
     
     private let credentialsJsonName = "optimove"
     
@@ -47,5 +48,68 @@ import NotificationCenter
         }
         
         Optimove.initialize(with: config.build())
+    }
+    
+    @objc(updateConsent:)
+    func updateConsent(command: CDVInvokedUrlCommand) {
+        OptimoveInApp.updateConsent(forUser: command.arguments[0] as? Bool ?? false)
+    }
+    
+    @objc(reportEvent:)
+    func reportEvent(command: CDVInvokedUrlCommand) {
+        let name = command.arguments[0] as! String
+        var parms = [String: Any]()
+        for i in 1..<command.arguments.count {
+            parms[command.arguments[i] as! String] = command.arguments[i + 1]
+        }
+        Optimove.shared.reportEvent(name: name, parameters: parms)
+    }
+    
+    @objc(reportScreenVisit:)
+    func reportScreenVisit(command: CDVInvokedUrlCommand) {
+        let screenTitle = command.arguments.first!
+        let screenCategory = command.arguments[1]
+        Optimove.shared.reportScreenVisit(screenTitle: screenTitle as! String, screenCategory: screenCategory as? String)
+    }
+    
+    @objc(registerUser:)
+    func registerUser(command: CDVInvokedUrlCommand) {
+        let sdkId = command.arguments.first!
+        let email = command.arguments[1]
+        Optimove.shared.registerUser(sdkId: sdkId as! String, email: email as! String)
+    }
+    
+    @objc(setUserId:)
+    func setUserId(command: CDVInvokedUrlCommand) {
+        Optimove.shared.setUserId(command.arguments.first as! String)
+    }
+    
+    @objc(getVisitorID:)
+    func getVisitorID(command: CDVInvokedUrlCommand) {
+        self.commandDelegate.run {
+            let pluginResult = CDVPluginResult(status: .ok, messageAs: Optimove.getVisitorID())
+            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        }
+    }
+    
+    @objc(setUserEmail:)
+    func setUserEmail(command: CDVInvokedUrlCommand) {
+        Optimove.shared.setUserEmail(email: command.arguments.first as! String)
+    }
+    
+    @objc(getInboxItems:)
+    func getInboxItems(command: CDVInvokedUrlCommand) {
+        self.commandDelegate.run {
+            let pluginResult = CDVPluginResult(status: .ok, messageAs: OptimoveInApp.getInboxItems())
+            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        }
+    }
+    
+    @objc(isAvailable:)
+    func isAvailable(command: CDVInvokedUrlCommand) {
+        self.commandDelegate.run {
+            let pluginResult = CDVPluginResult(status: .ok, messageAs: OptimoveInApp.getInboxItems())
+            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        }
     }
 }
