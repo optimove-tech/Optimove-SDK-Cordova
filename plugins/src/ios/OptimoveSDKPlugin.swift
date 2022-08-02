64f44754c8693487aa5999207a8cb3c71b1526f6
Barak <Barak_b@optimove.com>
2022-08-02 10:41:08 +0300

    - make config lazy for thread safety

--- plugins/src/ios/OptimoveSDKPlugin.swift
+++ plugins/src/ios/OptimoveSDKPlugin.swift
@@ -1,49 +1,33 @@
 import OptimoveSDK
 import NotificationCenter
 @objc(Optimove_Cordova) class OptimoveSDKPlugin : CDVPlugin {
-    private let internalQueue = DispatchQueue(label: "com.singletioninternal.queue",
-                                              qos: .default,
-                                              attributes: .concurrent)
-    
-//    private var pendingPush: PushNotification?
-    
     private let optimoveCredentialsKey = "optimoveCredentials"
     private let optimoveMobileCredentialsKey = "optimoveMobileCredentials"
     
-    func load() {
-        internalQueue.sync(flags: .barrier) {
-            NotificationCenter.default.addObserver(self, selector: #selector(self.didFinishLaunching(notification:)), name: UIApplication.didFinishLaunchingNotification, object: nil)
-        }
-    }
-    
-    @objc func didFinishLaunching(notification: NSNotification) {
+    private lazy var config: OptimoveConfig? = {
         let configPath = Bundle.main.path(forResource: "optimove", ofType: "plist")
         
         guard let configPath = configPath else {
-            print("optimove.plist NOT FOUND");
-            return
+            print("optimove.plist NOT FOUND")
+            return nil
         }
         
         guard let configValues: [String: String] = NSDictionary(contentsOfFile: configPath) as? [String: String] else {
-            print("optimove.plist IS NOT VALID");return
+            print("optimove.plist IS NOT VALID")
+            return nil
         }
         
-//        if let userInfo = notification.userInfo {
-//            let userInfoDict = notification.userInfo?[UIApplication.LaunchOptionsKey.remoteNotification];
-//            if userInfoDict != nil {
-//                pendingPush = PushNotification.init(userInfo: userInfo, response: nil)
-//            }
-//        }
-        
         let config = OptimoveConfigBuilder(optimoveCredentials: configValues[optimoveCredentialsKey], optimobileCredentials: configValues[optimoveMobileCredentialsKey])
-//        
-//        if configValues["inAppConsentStrategy"] == "auto-enroll" {
-//            config.enableInAppMessaging(inAppConsentStrategy: .autoEnroll)
-//        }
-//        else if configValues["inAppConsentStrategy"] == "explicit-by-user" {
-//            config.enableInAppMessaging(inAppConsentStrategy: .explicitByUser)
-//        }
         
-        Optimove.initialize(with: config.build())
+        return config.build()
+    }()
+    
+    func load() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didFinishLaunching(notification:)), name: UIApplication.didFinishLaunchingNotification, object: nil)
+    }
+    
+    @objc func didFinishLaunching(notification: NSNotification) {
+        guard let config = config else { return }
+        Optimove.initialize(with: config)
     }
 }
