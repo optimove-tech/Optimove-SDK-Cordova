c072c0af27f2806206667dd13a8ae08d4fd9eb67
vvoicehovics <vladislav_v@optimove.com>
2022-09-08 10:55:26 +0100

    correctly parse enableDeferredDeepLinking flag

--- plugins/scripts/inject-config.js
+++ plugins/scripts/inject-config.js
@@ -3,24 +3,22 @@ const fs = require('fs');
 const path = require('path');
 
 function createJsonWithDefaultValues(optimoveCredentials, optimoveMobileCredentials , inAppConsentStrategy, enableDeferredDeepLinking) {
-      return {
-        OPTIMOVE_CREDENTIALS:
-          !isEmpty(optimoveCredentials) &&
-          isString(optimoveCredentials)
-            ? optimoveCredentials
-            : "",
-        OPTIMOVE_MOBILE_CREDENTIALS:
-          !isEmpty(optimoveMobileCredentials) &&
-          isString(optimoveMobileCredentials)
-            ? optimoveMobileCredentials
-            : "",
-        IN_APP_STRATEGY: inAppConsentStrategy,
-         ENABLE_DEFERRED_DEEP_LINKING:
-      !isEmpty(enableDeferredDeepLinking) && isString(enableDeferredDeepLinking)
-        ? enableDeferredDeepLinking
-        : "false"
-      };
+  return {
+    OPTIMOVE_CREDENTIALS:
+      !isEmpty(optimoveCredentials) &&
+      isString(optimoveCredentials)
+        ? optimoveCredentials
+        : "",
+    OPTIMOVE_MOBILE_CREDENTIALS:
+      !isEmpty(optimoveMobileCredentials) &&
+      isString(optimoveMobileCredentials)
+        ? optimoveMobileCredentials
+        : "",
+    IN_APP_STRATEGY: inAppConsentStrategy,
+    ENABLE_DEFERRED_DEEP_LINKING:
+      enableDeferredDeepLinking === 'true' || enableDeferredDeepLinking === true
  }
+}
 
 function hasPlatform(context, platform) {
   return context.opts.platforms.indexOf(platform) > -1;
@@ -85,7 +83,6 @@ function prepareAndroid(context, optimoveConfig) {
     "values",
     "optimove.xml"
   );
-
   const config = renderTemplate(
     "optimove.xml",
     createJsonWithDefaultValues(
