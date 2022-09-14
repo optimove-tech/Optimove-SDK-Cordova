db847c07d9a120abdd76cf33f0285652fb512080
Tom Barzilay <tom_b@optimove.com>
2022-09-14 17:49:38 +0300

    added push notifications related interface

---
+++ plugins/src/core/push.ts
@@ -0,0 +1,7 @@
+export interface PushNotification {
+    id: number;
+    title?: string | null;
+    message?: string | null;
+    data?: { [key: string]: any } | null;
+    url?: string | null;
+}
