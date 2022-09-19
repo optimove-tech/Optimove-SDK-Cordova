d7b6a8c91ac7f7171eec01f1c7b07a431d0d4858
Tom Barzilay <tom_b@optimove.com>
2022-09-19 16:51:59 +0300

    fixed pushNotification interface

--- plugins/src/core/push.ts
+++ plugins/src/core/push.ts
@@ -1,7 +1,7 @@
 export interface PushNotification {
   id: number;
-  title?: string | null;
-  message?: string | null;
-  data?: { [key: string]: any } | null;
-  url?: string | null;
+  title: string | null;
+  message: string | null;
+  data: { [key: string]: any } | null;
+  url: string | null;
 }
