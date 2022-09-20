32b2a39b7ebc65f50ba0305bda9fe83a87065711
Tom Barzilay <tom_b@optimove.com>
2022-09-20 14:58:01 +0300

    deleted log import

--- plugins/src/android/OptimoveSDKPlugin.java
+++ plugins/src/android/OptimoveSDKPlugin.java
@@ -20,7 +20,6 @@ import com.optimove.android.optimobile.PushMessage;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.LOG;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
